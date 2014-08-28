63ecedba0ce6130a7ca00b413a20ee81aef7e2de
leo.adams <leo@leo.adams>
2014-08-29 05:58:57 +0800

    quick_flush

--- web01/backend/s_analyzer.py
+++ web01/backend/s_analyzer.py
@@ -22,16 +22,19 @@ def get_region_ip_list(ip_list):
 def raw_decrease_ip_list(ip_list):
     """docstring for alex_method"""
     ip_dic = {}
+    decreased_ip_list = []
     for i in ip_list:
         if ip_dic.has_key(i):
             ip_dic[i] += 1
         else :
             ip_dic[i] = 1
-    decreased_ip_list = sorted(ip_dic.items(), key=lambda x:x[1])[:100]
+    #decreased_ip_list = sorted(ip_dic.items(), key=lambda x:x[1])[:100]
+    for i in ip_dic.items:
+        decreased_ip_list.append(i)
     return decreased_ip_list
 
 def get_ip_list_from_log(logfile):
-    global error_line_num
+    error_line_num = 0
     ip_list = []
     f = file(logfile)
     for raw_line in f.xreadlines():
@@ -45,6 +48,7 @@ def get_ip_list_from_log(logfile):
             #sys.exit()
             continue
         ip_list.append(internet_ip)
+    print 'error line number',error_line_num
     return ip_list
 
 def get_province_list(ip_list):
@@ -52,17 +56,17 @@ def get_province_list(ip_list):
     province_dic = {}
     province_list = []
     for i in ip_list:
-        chosen_ip_info = ipsearch.IPSearch(i[1])
+        chosen_ip_info = ipsearch.IPSearch(i[0])
         #province,city = line_items[3].encode('gb2312'),line_items[4].encode('gb2312')
         province,city = chosen_ip_info[3],chosen_ip_info[4]
         if province_dic.has_key(province):
-            province_dic[province] += i[0]
+            province_dic[province] += i[1]
         else:
-            province_dic[province] = i[0]
+            province_dic[province] = i[1]
     sorted_province_list = sorted(province_dic.items(), key=lambda x:x[1], reverse = True)
     return sorted_province_list
 
-def write_to_excel():
+def write_to_excel(province_list):
     f = csv.writer(open("raw_test.csv","wb+"))
     f.writerow(["province","click"])
     for i in province_list:
@@ -71,8 +75,11 @@ def write_to_excel():
 
 if __name__ == '__main__':
     ip_list = get_ip_list_from_log('log25w.log')
-    #region_ip_list = get_region_ip_list(ip_list)
-    raw_decreased_ip_list = raw_decrease_ip_list(ip_list)
-    province_list = get_province_list(raw_decreased_ip_list)
+    print 'total ip number: ',len(ip_list)
+    #decreased_ip_list = get_region_ip_list(ip_list)
+    decreased_ip_list = raw_decrease_ip_list(ip_list)
+    print 'choose ',len(decreased_ip_list),'ip to search ip'
+    province_list = get_province_list(decreased_ip_list)
+    print 'related province number: ',len(province_list)
     write_to_excel(province_list)
-    print '\nerror line number: ',error_line_num
+    print 'have writen to excel'
